e2334890466ef2986c983b7d6b3a73a0cc0256ce
sparkerhoney <lhe339@gmail.com>
2023-10-29 22:31:46 +0900

    Preprocess

--- model/DataProcessor.py
+++ model/DataProcessor.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 class DataProcessor:
     
@@ -18,6 +19,24 @@ class DataProcessor:
         }
         return tier_dict.get(tier, -1)
 
+    @staticmethod
+    def convert_tier_to_exponential_weight(tier):
+        tier_dict = {
+            'unranked': 0,
+            'bronze': 1,
+            'silver': 2,
+            'gold': 3,
+            'platinum': 4,
+            'diamond': 5,
+            'master': 6
+        }
+        tier_numeric = tier_dict.get(tier, -1)
+        return np.exp(tier_numeric)
+    
+    def add_tier_exponential_weight(self):
+        self.data['tier_exp_weight'] = self.data['tier'].apply(self.convert_tier_to_exponential_weight)
+        return self.data
+
     def calculate_team_features(self):
         return self.data.groupby(['matchid', 'teamid']).agg({
             'mmr': 'mean',
@@ -29,7 +48,6 @@ class DataProcessor:
     def preprocess(self):
         match_team_features = self.calculate_team_features()
         self.data = self.data.merge(match_team_features, on=['matchid', 'teamid'], suffixes=('', '_team_avg'))
-        self.data = self.guild_avg()
         self.data = self.normalize_column('accumatches')
         self.data = self.compute_team_stats()
         self.data = self.compute_recent_performance_index()
@@ -38,15 +56,44 @@ class DataProcessor:
         self.data = self.compute_recent_winprob_stats()
         self.data = self.apply_tier_conversion_and_compute_average()
         self.data = self.compute_streak_rate()
+        self.data = self.guild_mean()
+        self.data = self.guild_median()
+        self.data = self.guild_mode()
+        self.data = self.add_tier_exponential_weight()
 
-    def guild_avg(self):
+    # guild_membership
+    def guild_mean(self):
         df = self.data.copy()
         df['guildid'].fillna('NoGuild', inplace=True)
         df['tier_numeric'] = df['tier'].map(self.convert_tier_to_numeric)
-        guild_avg = df.groupby('guildid')[['mmr', 'winstreak', 'recentwinprob', 'accumatches', 'tier_numeric']].mean()
-        new_columns = {col: f'{col}_guild_avg' for col in guild_avg.columns}
-        guild_avg.rename(columns=new_columns, inplace=True)
-        df = pd.merge(df, guild_avg, on='guildid', how='left')
+        guild_mean = df.groupby('guildid')[['mmr', 'winstreak', 'recentwinprob', 'accumatches', 'tier_numeric']].mean()
+        new_columns = {col: f'{col}guild_mean' for col in guild_mean.columns}
+        guild_mean.rename(columns=new_columns, inplace=True)
+        df = pd.merge(df, guild_mean, on='guildid', how='left')
+        return df
+    
+    def guild_median(self):
+        df = self.data.copy()
+        df['guildid'].fillna('NoGuild', inplace=True)
+        df['tier_numeric'] = df['tier'].map(self.convert_tier_to_numeric)
+        guild_median = df.groupby('guildid')[['mmr', 'winstreak', 'recentwinprob', 'accumatches', 'tier_numeric']].median()
+        new_columns = {col: f'{col}guild_median' for col in guild_median.columns}
+        guild_median.rename(columns=new_columns, inplace=True)
+        df = pd.merge(df, guild_median, on='guildid', how='left')
+        return df
+    
+    def guild_mode(self):
+        df = self.data.copy()
+        df['guildid'].fillna('NoGuild', inplace=True)
+        df['tier_numeric'] = df['tier'].map(self.convert_tier_to_numeric)
+        
+        def calculate_mode(group):
+            return group.mode().iloc[0]
+        
+        guild_mode = df.groupby('guildid')[['mmr', 'winstreak', 'recentwinprob', 'accumatches', 'tier_numeric']].apply(calculate_mode)
+        new_columns = {col: f'{col}guild_mode' for col in guild_mode.columns}
+        guild_mode.rename(columns=new_columns, inplace=True)
+        df = pd.merge(df, guild_mode, on='guildid', how='left')
         return df
 
     def normalize_column(self, column):
